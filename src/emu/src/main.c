b0b16fb8055c55ddcebdb4ab01d93d07ca74b4b9
William Batts III <william.batts.iii@gmail.com>
2024-09-25 06:01:32 -0400

    working on emulator

--- src/emu/src/main.c
+++ src/emu/src/main.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "cpu.h"
 
 
 #define MEMORY_SIZE_KB 32768 // 32 megabytes
+#define PROC_SPEED_HZ 12500000 // 12.5 MHz
 
-#define FPS 60 // probably gonna be useful for when I get a display library set up
+#define FPS 60 // frames per second
 #define TPF 1 // ticks per frame
-#define TPS (FPS * TPF) // ticks per second is nice to know
+#define TPS (FPS * TPF) // ticks per second
+#define CPF (PROC_SPEED_HZ / TPS)
+
 
 uint8_t* memory;
 
 int main(void) {
     memory =  malloc(MEMORY_SIZE_KB * 1024 * sizeof(uint8_t)); // TODO: program arguments to change size at runtime
+    printf("%d\n", CPF);
     return 0;
 }
